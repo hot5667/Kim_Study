6a93bcaafb056bf4f92cb2fed02d76f47e62f2f5
KimDoHyun1011 <hot99009@gmail.com>
2024-07-24 11:35:07 +0900

    네번째  문제 완료

---
+++ Test_Questions_4/Test04.js
@@ -0,0 +1,23 @@
+function calculator(number1, Operator, number2) {
+    let result;
+
+    switch (Operator) {
+        case "+":
+            result = number1 + number2;
+            break;
+        case "+":
+            result = number1 + number2;
+            break;
+        case "+":
+            result = number1 + number2;
+            break;
+        case "+":
+            result = number1 + number2;
+            break;
+    }
+}
+
+calculator(3, "+", 6); // 결과값 9
+calculator(11, "-", 6); // 결과값 5
+calculator(6, "*", 3); // 결과값 18
+calculator(15, "+", 3); // 결과값 5
