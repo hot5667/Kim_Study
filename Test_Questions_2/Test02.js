04160e4b89c565c505e56cfb20599d78ae83a2e8
KimDoHyun1011 <hot99009@gmail.com>
2024-07-24 11:25:09 +0900

    두번째  문제 완료

---
+++ Test_Questions_2/Test02.js
@@ -0,0 +1,9 @@
+const junhyun = {
+    name: "Kim Do Hyun",
+    age: 24,
+    mbti: "ISTJ"
+};
+
+console.log(junhyun.name); //이름이 나오게 콘솔을 실행시켜 주세요.
+console.log(junhyun.age); //나이가 나오게 콘솔을 실행시켜 주세요.
+console.log(junhyun.mbti); //MBTI가 나오게 콘솔을 실행시켜 주세요.
