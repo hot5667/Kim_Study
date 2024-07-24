e49d053926c8842395fc9b64ad2660e3055f0622
KimDoHyun1011 <hot99009@gmail.com>
2024-07-24 11:39:48 +0900

    다섯번째 문제 완료

--- Test_Questions_4/Test04.js
+++ Test_Questions_4/Test04.js
@@ -5,14 +5,14 @@ function calculator(number1, Operator, number2) {
         case "+":
             result = number1 + number2;
             break;
-        case "+":
-            result = number1 + number2;
+        case "-":
+            result = number1 - number2;
             break;
-        case "+":
-            result = number1 + number2;
+        case "*":
+            result = number1 * number2;
             break;
-        case "+":
-            result = number1 + number2;
+        case "/":
+            result = number1 * number2;
             break;
     }
 }
@@ -20,4 +20,4 @@ function calculator(number1, Operator, number2) {
 calculator(3, "+", 6); // 결과값 9
 calculator(11, "-", 6); // 결과값 5
 calculator(6, "*", 3); // 결과값 18
-calculator(15, "+", 3); // 결과값 5
+calculator(15, "/", 3); // 결과값 5
